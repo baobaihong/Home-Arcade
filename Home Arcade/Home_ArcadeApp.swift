cba62e43cb3de4899bcfff7b0a1c82e395e5f737
Jason Bao <baihongbao@outlook.com>
2024-11-10 10:37:52 +0800

    Initial Commit

---
+++ Home Arcade/Home_ArcadeApp.swift
@@ -0,0 +1,33 @@
+//
+//  Home_ArcadeApp.swift
+//  Home Arcade
+//
+//  Created by Jason on 2024/11/10.
+//
+
+import SwiftUI
+
+@main
+struct Home_ArcadeApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
